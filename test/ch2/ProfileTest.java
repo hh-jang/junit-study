a2e4d4a35b2ebebc65285bfb2c60cbe8bef42977
hh-jang <jhh1117@daum.net>
2020-05-10 17:18:21 +0900

    chapter 2 진행 - 테스트코드 추가
    * 가중치의 값이 DontCare일때에 적합하지 않아도 match가 되는지 확인을 위한 테스트코드 추가

--- test/ch2/ProfileTest.java
+++ test/ch2/ProfileTest.java
@@ -23,4 +23,21 @@ public class ProfileTest {
 
         assertEquals(false, matches);
     }
+
+    @Test
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        Profile profile = new Profile("Bull Hockey, Inc");
+        Question question = new BooleanQuestion(1, "Got Milk?");
+        Answer profileAnswer = new Answer(Bool.FALSE, question);
+        profile.add(profileAnswer);
+
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(Bool.TRUE, question);
+        Criterion criterion = new Criterion(Weight.DontCare, criteriaAnswer);
+        criteria.add(criterion);
+
+        boolean match = profile.matches(criteria);
+
+        assertEquals(true, match);
+    }
 }
